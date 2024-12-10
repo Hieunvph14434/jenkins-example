590f6213fc7dad42d339b4744a2784c330ceb0e0
hieunv <hieunv@sabitech.vn>
2024-12-10 17:23:19 +0700

    daskl

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/about', function (req, res) {
-    // init 111 888111
+    // init 111 888111s
   res.send('Hello World 999')
 })
 
