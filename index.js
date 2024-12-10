136febfc1274d7f873c5bfa7f3674756a948e0a8
hieunv <hieunv@sabitech.vn>
2024-12-10 15:20:07 +0700

    change route

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.get('/contact', function (req, res) {
-  res.send('Hello World')
+app.get('/about', function (req, res) {
+  res.send('Hello World 999')
 })
 
 app.listen(3003)
