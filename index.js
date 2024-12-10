4f43da0faf0a58dd602a783f4404eaa6e52e6f8d
hieunv <hieunv@sabitech.vn>
2024-12-10 18:03:04 +0700

    test5

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/about', function (req, res) {
-    // init 111 888111ssa
+    // init 111 888111ssa test 5
   res.send('Hello World 999')
 })
 
