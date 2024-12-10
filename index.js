dce32e4029029ed586af050d96840b6bcce5ddc5
hieunv <hieunv@sabitech.vn>
2024-12-10 16:07:23 +0700

    change text

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.get('/about', function (req, res) {
+app.get('/aboutHome', function (req, res) {
   res.send('Hello World 999')
   console.log('123');
-  
 })
 
 app.listen(3003)
