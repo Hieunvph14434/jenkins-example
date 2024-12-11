6e9e8588ad99126a3a9b53be7fd7d34f3824e733
hieunv <hieunv@sabitech.vn>
2024-12-11 16:34:46 +0700

    testa

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,4 +6,14 @@ import pluginJs from "@eslint/js";
 export default [
   {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
+  {
+    rules: {
+        semi: ["error", "never"]
+    }
+  },
+  {
+    rules: {
+        semi: ["warn", "always"]
+    }
+  }
 ];
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/about', function (req, res) {
     // init 111 888111ssa
   let hello = "hello";
-  res.send('Hello World 999', hello)
-})
+  console.log(hello)
+  
+  res.send('Hello World 999');
+});
 
-app.listen(3003)
+app.listen(3003);
