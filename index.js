1ae05cdef44837d8b34c0d775f82e5222cdbdabe
hieunv <hieunv@sabitech.vn>
2024-12-11 16:50:47 +0700

    add file ignores eslint

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.get('/about', function (req, res) {
     // init 111 888111ssa
   let hello = "hello";
-  // console.log(hello)
+  console.log(hello)
   
   res.send('Hello World 999');
 });
