57150ea454f5f7f32f3b482b6fea1065d46ea48a
Hieunvph14434 <101816215+Hieunvph14434@users.noreply.github.com>
2024-12-11 15:23:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.get('/about', function (req, res) {
     // init 111 888111ssa
-  let hello = "hello";
+  let hello2 = "hello";
   res.send('Hello World 999', hello2)
 })
 
